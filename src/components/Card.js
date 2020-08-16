0ac0d01d6a5c777ebefdd6fe74c7891684c15ac4
Haikel Fazzani <haikel.fazzani@hotmail.com>
2020-08-16 23:45:44 +0100

    update app

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,21 +6,24 @@ let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator
 // moz-page-thumb://thumbnails/?url=https%3A%2F%2Fpensive-poincare-29c3c6.netlify.app%2F&revision=2050
 let isDev = true;
 
-export default function Card ({ id, title, url, onClick }) {
- 
+export default function Card ({ id, title, url }) {
+
   return (
-    <div className="h-100 card d-flex-col" data-id={id}>
+    <div className="h-100 card" data-id={id}>
 
-      <img src={
-        isDev
-          ? placeImg
-          : isChrome
-            ? "chrome://favicon/size/48/" + url
-            : "moz-page-thumb://thumbnails/?url=" + url
-      }
-        onClick={onClick} alt={title} />
+      <a href={url} className="d-flex-col">
+        <img
+          alt={title}
+          src={
+            isDev
+              ? placeImg
+              : isChrome
+                ? "chrome://favicon/size/48/" + url
+                : "moz-page-thumb://thumbnails/?url=" + url
+          } />
 
-      <a href={url} className="truncate">{title}</a>
+        <span className="truncate">{title}</span>
+      </a>
     </div>
   );
 }
