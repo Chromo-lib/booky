4083f78fba6f508e1d1f86d96c9aa149a8b9e3cf
haikelfazzani <haikel.fazzani@hotmail.com>
2023-02-24 16:32:30 +0100

    2.0.1

--- src/events/onReset.ts
+++ src/events/onReset.ts
@@ -1,7 +1,9 @@
 export default function onReset(e: any) {
   if (window.confirm('Do you really want to reset?')) {
-    if (e.target.id.includes('settings')) localStorage.removeItem('settings')
-    else localStorage.removeItem('bgOptions')
+    if (e.target.id.includes('settings')) localStorage.removeItem('settings');
+    else localStorage.removeItem('bgOptions');
+
+
+    window.location.reload()
   }
-  window.location.reload()
 }
--- src/utils/setTabBg.ts
+++ src/utils/setTabBg.ts
@@ -1,17 +1,17 @@
 import store from "../store";
 import { IBgOptions } from "../types";
 
-export default function setTabBg(bgOptions: IBgOptions) {  
+export default function setTabBg(bgOptions: IBgOptions) {
   store.actions.setBgOptions(bgOptions)
-  
+
   const base64String = localStorage.getItem('wallpaper');
 
   if (base64String && bgOptions && bgOptions.image) {
     document.body.style.background = bgOptions.widthFiler
-      ? `linear-gradient(135deg,rgb(33 37 41 / 65%),rgb(33 37 41 / 72%)),url(data:image/png;base64,${base64String})`
-      : `url(data:image/png;base64,${base64String})`;
+      ? `linear-gradient(135deg,rgb(33 37 41 / 65%),rgb(33 37 41 / 72%)),url(data:image/png;base64,${base64String}) no-repeat center center fixed`
+      : `url(data:image/png;base64,${base64String}) no-repeat center center fixed`;
   }
 
   if (bgOptions && bgOptions.widthBlur) document.body.classList.add('backdrop-blur');
-  else document.body.classList.remove('backdrop-blur');  
+  else document.body.classList.remove('backdrop-blur');
 }
