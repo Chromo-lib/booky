fa4a9abea2e7dc8a60c86dd405c2eef03c25194e
haikelfazzani <haikel.fazzani@hotmail.com>
2023-04-24 20:17:46 +0100

    2.0.6

--- src/main.ts
+++ src/main.ts
@@ -86,7 +86,7 @@ const onLoad = async () => {
         b.children?.forEach(v => {
           if(!v.children) ul.innerHTML += `<li class="d-flex align-center border-bottom">
           <img class="mr-2 rounded" width="20" height="20" src="${faviconURL(v.url as string)}" alt="${v.title}" />  
-          <a href="${v.url}" target="_blank">${v.title}</a>
+          <a href="${v.url}" title="${v.title}" target="_blank">${v.title}</a>
           </li>`
         })
 
